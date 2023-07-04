8a4f4027b11bf31b76406fb6ad200e670e509f93
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-04 11:13:08 +0300

    latest

--- app/page.js
+++ app/page.js
@@ -1,95 +1,37 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+// Component
+// Functional Component - Class Based Component
+// <div class="x"> </div>
+
+// JSX içerisinde javascript yazmak istediğimizde {} kullanıyoruz
 export default function Home() {
+  let name = "Halit"; // API isteği?
+  // Token'i decode edip?
+
+  let cars = ["BMW", "TOGG", "Mercedes", "Fiat"]
+
+
+  let clickFunction = () => {
+    alert("Butona tıklandı.")
+  }
+  // forEach
+  // map
+  // jsx içerisinde iterasyonlar her zaman map ile yapılmalı.
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    <>
+      <div>
+        Merhaba, hoşgeldiniz {name}
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <ul>
+            {cars.map(car => <li>{car}</li>)}
+          </ul>
         </div>
+        <button onClick={() => {alert("Butona tıklandı.")}}>Uyarı</button>
+        <button onClick={clickFunction}>Uyarı</button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div></div>
+    </>
   )
 }
