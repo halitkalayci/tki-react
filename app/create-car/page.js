d1f7e344948906b8e4b3a2a60331c71d82a85fba
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-07 14:57:20 +0300

    regex for turkish plate

--- app/create-car/page.js
+++ app/create-car/page.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import styles from "../page.module.css"
 import * as Yup from "yup";
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { Button } from 'primereact/button';
 function CreateCar() {
 
@@ -14,7 +14,8 @@ function CreateCar() {
     //TODO: fix regex
     const addCarValidationSchema = Yup.object().shape({
         kilometer: Yup.number().required().moreThan(0), // >
-        plate: Yup.string().required().min(2).matches(new RegExp("(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2}))")) // >=
+        plate: Yup.string().required().min(2)
+        .matches(/(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2}))/, 'Lütfen düzgün bir Türkiye plakası giriniz.'),
     })
 
     return (
@@ -30,10 +31,12 @@ function CreateCar() {
                 <div className='form-group'>
                     <label>Kilometre</label>
                     <Field name="kilometer" className="form-control"></Field>
+                    <ErrorMessage name='kilometer'></ErrorMessage>
                 </div>
                 <div className='form-group'>
                     <label>Plaka</label>
                     <Field name="plate" className="form-control"></Field>
+                    <ErrorMessage name='plate'></ErrorMessage>
                 </div>
                 <div>
                     <Button type='submit' label='Ekle' severity='info' className='w-100'></Button>
