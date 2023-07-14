41ec400ff0618e05bab63c5f767dc43a4e477185
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-14 14:29:01 +0300

    exports

--- app/reports/page.js
+++ app/reports/page.js
@@ -1,12 +1,23 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "../page.module.css"
 import axiosInstance from '../utilities/axiosInterceptors';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
+import {MultiSelect} from "primereact/multiselect"
 function Reports() {
+    const cols = [
+        { field: 'id', header: 'ID' },
+        { field: 'kilometer', header: 'Kilometer' },
+        { field: 'plate', header: 'Plate' },
+        { field: 'image', header:'Image'}
+    ];
   const [cars, setCars] = useState([])
+  const [selectedCars, setSelectedCars] = useState([])
+  const [selectedColumns, setSelectedColumns] = useState([])
+  const [visibleColumns, setVisibleColumns] = useState(cols)
+  const dt = useRef();
   useEffect(() =>{
     fetchCars();
   }, [])
@@ -17,18 +28,14 @@ function Reports() {
     })
   }
 
-  const cols = [
-    { field: 'id', header: 'ID' },
-    { field: 'kilometer', header: 'Kilometer' },
-    { field: 'plate', header: 'Plate' },
-];
+
 
   const exportPdf = () => {
     import('jspdf').then((jsPDF) => {
         import('jspdf-autotable').then(() => {
             const doc = new jsPDF.default(0, 0);
             const exportColumns = cols.map((col) => ({ title: col.header, dataKey: col.field }));
-            doc.autoTable(exportColumns, cars);
+            doc.autoTable(exportColumns, cars,{margin:{top:25}});
             console.log(doc);
             var img = new Image()
             img.src = "logo.png";
@@ -38,6 +45,10 @@ function Reports() {
     });
   }
 
+  const exportToCsv = () => {
+    dt.current.exportCSV({ selectionOnly:true });
+  }
+
 
   const imageColumnTemplate = (row) => {
     return <img className='table-img' src={row.image}></img>
@@ -45,10 +56,22 @@ function Reports() {
 
   const headerTemplate = () => {
     return <div> 
+        <MultiSelect value={visibleColumns} options={cols} optionLabel="header" onChange={onColumnToggle} className="w-full sm:w-20rem" display="chip" />
         <Button onClick={exportExcel} label='Export Excel' severity='info'></Button> 
         <Button onClick={exportPdf} label='Export PDF' severity='success' className='mx-2'></Button>
+        <Button onClick={exportToCsv} label='Export CSV' severity='danger' className='mx-2'></Button>
+        <Button onClick={() => { console.log(selectedColumns) }} label='Seçilileri Yazdır' severity='danger' className='mx-2'></Button>
         </div>
   }
+
+  const onColumnToggle = (event) => {
+    let selectedColumns = event.value;
+    let orderedSelectedColumns = cols.filter((col) => selectedColumns.some((sCol) => sCol.field === col.field));
+
+    setVisibleColumns(orderedSelectedColumns);
+};
+
+
 
   const saveAsExcelFile = (buffer, fileName) => {
     import('file-saver').then((module) => {
@@ -66,6 +89,7 @@ function Reports() {
   const exportExcel = () => {
     import('xlsx').then((xlsx) => {
         // databaseden raporlanacak veriyi tekrar al.
+        // kullanıcı bunu seçtimi?
         let dataToExport = cars.map(i=> { return {id:i.id, kilometer:i.kilometer, plate:i.plate, kurum:"TKI"}  })
         const worksheet = xlsx.utils.json_to_sheet(dataToExport);
         const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
@@ -84,12 +108,45 @@ function Reports() {
   // Client Side Pagination
   return (
     <main className={styles.main}>
-        <DataTable header={headerTemplate} value={cars} paginator rows={5} rowsPerPageOptions={[5,10,20,30]}>
-            <Column field='id' header="#"></Column>
-            <Column field='kilometer' header="Kilometre"></Column>
-            <Column field='plate' header="Plaka"></Column>
-            <Column field='image' header="Image" body={imageColumnTemplate}></Column>
+        <DataTable selectionMode='checkbox' selection={selectedCars} onSelectionChange={(e) => setSelectedCars(e.value)}  ref={dt} header={headerTemplate} value={cars} paginator rows={5} rowsPerPageOptions={[5,10,20,30]}>
+        <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+            {visibleColumns.map(col=> 
+            {
+                return <Column field={col.field} key={col.field} header={col.header}></Column>
+            })}
         </DataTable>
+
+    <div>
+        <div className='d-flex flex-direction-column'>
+        <label className='mx-2'>ID</label>
+        <input onChange={(e) => {
+            if(e.target.checked == true){
+                setSelectedColumns([...selectedColumns,"id"])
+            }else{
+                setSelectedColumns((oldColumns) => oldColumns.filter(i=>i != "id"))
+            }}} className='form-check' type="checkbox"  />
+        </div>
+        <div className='d-flex flex-direction-column'>
+        <label className='mx-2'>KM</label>
+        <input
+         onChange={(e) => {
+            console.log(e);
+            if(e.target.checked == true){
+                setSelectedColumns([...selectedColumns,"kilometer"])
+            }else{
+                setSelectedColumns((oldColumns) => oldColumns.filter(i=>i != "kilometer"))
+            }}}
+             className='form-check' type="checkbox"  />
+        </div>
+        <div className='d-flex flex-direction-column'>
+        <label className='mx-2'>Plate</label>
+        <input className='form-check' type="checkbox"  />
+        </div>
+        <div className='d-flex flex-direction-column'>
+        <label className='mx-2'>Image</label>
+        <input className='form-check' type="checkbox"  />
+        </div>
+    </div>
     </main>
   )
 }
