3f4c180cc1a52528bc041e80481b9cc51e3bec30
halitkalayci <halit.kalayci@ext.etiya.com>
2023-07-14 11:42:14 +0300

    export excel

--- app/reports/page.js
+++ app/reports/page.js
@@ -4,6 +4,7 @@ import styles from "../page.module.css"
 import axiosInstance from '../utilities/axiosInterceptors';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
 function Reports() {
   const [cars, setCars] = useState([])
   useEffect(() =>{
@@ -21,10 +22,45 @@ function Reports() {
     return <img className='table-img' src={row.image}></img>
   }
 
+  const headerTemplate = () => {
+    return <div> <Button onClick={exportExcel} label='Export Excel' severity='info'></Button> </div>
+  }
+
+  const saveAsExcelFile = (buffer, fileName) => {
+    import('file-saver').then((module) => {
+        if (module && module.default) {
+            let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+            let EXCEL_EXTENSION = '.xlsx';
+            const data = new Blob([buffer], {
+                type: EXCEL_TYPE
+            });
+
+            module.default.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        }
+    });
+};
+
+  const exportExcel = () => {
+    import('xlsx').then((xlsx) => {
+        // databaseden raporlanacak veriyi tekrar al.
+        console.log(cars);
+        const worksheet = xlsx.utils.json_to_sheet(cars);
+        const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+        const excelBuffer = xlsx.write(workbook, {
+            bookType: 'xlsx',
+            type: 'array'
+        });
+
+        saveAsExcelFile(excelBuffer, 'cars');
+    });
+  }
+
+
+  // npm install xlsx jspdf jspdf-autotable
   // Client Side Pagination
   return (
     <main className={styles.main}>
-        <DataTable value={cars} paginator rows={5} rowsPerPageOptions={[5,10,20,30]}>
+        <DataTable header={headerTemplate} value={cars} paginator rows={5} rowsPerPageOptions={[5,10,20,30]}>
             <Column field='id' header="#"></Column>
             <Column field='kilometer' header="Kilometre"></Column>
             <Column field='plate' header="Plaka"></Column>
